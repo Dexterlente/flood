29dff1d163efbead8bdde57f4cd81426c68f448f
dexter <dexter.lente18@gmail.com>
2025-09-05 00:10:10 +0800

    ermove paht and onlick on nav

--- flood/app/_components/NavBar.tsx
+++ flood/app/_components/NavBar.tsx
@@ -1,15 +1,11 @@
 'use client'
 import React from 'react'
-import { useRouter } from 'next/navigation'
 
 
 const NavBar = () => {
-    const router = useRouter()
-
   return (
     <div 
-    onClick={() => router.push('/')}  
-    className="flex justify-center items-center h-16 bg-gray-100 font-bold text-xl cursor-pointer">
+    className="flex justify-center items-center h-16 bg-gray-100 font-bold text-xl">
       FLOOD CONTROL PROJECTS
     </div>
   )
